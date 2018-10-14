279d0f65a99cb18efbc9aad786e43ba025cacfcc
giuliojiang <giuliojiang@gmail.com>
2018-10-14 17:24:41 +0100

    Customize configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/app/app.tsx',
+  entry: './src/app/app.ts',
   plugins: [
     new CleanWebpackPlugin(['public/build'])
   ],
